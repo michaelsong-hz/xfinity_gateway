dc0e5952b8544e5bfcc0250fc0d5200a6ca60eaa
Michael Song <hello@michaelsong.me>
2024-10-16 19:30:50 -0400

    Updated script to authenticate on latest router version

--- src/xfinity_gateway.py
+++ src/xfinity_gateway.py
@@ -2,12 +2,25 @@ from bs4 import BeautifulSoup as BS
 import requests
 
 TIMEOUT = 30
+CONNECTED_DEVICES_PATH = '/connected_devices_computers.jst'
+LOGIN_PATH = '/check.jst'
 
 class XfinityGateway:
 
-    def __init__(self, host):
+    def __init__(self, host, username, password):
         self.host = 'http://' + host
+        self.username = username
+        self.password = password
+
         self.last_results = {}
+        self.session = requests.session()
+
+    def login(self):
+        data = {
+            'username': self.username,
+            'password': self.password,
+        }
+        self.session.post(self.host + LOGIN_PATH, data=data, timeout=TIMEOUT)
 
     def scan_devices(self):
         self._update_info()
@@ -17,16 +30,24 @@ class XfinityGateway:
         return self.last_results.get(device)
 
     def _update_info(self):
-        raw_html = requests.get(self.host, timeout=TIMEOUT).text
-        self._get_connected_devices(raw_html)
-
-    def _get_connected_devices(self, html):
+        soup = self._get_devices_soup()
         try:
-            soup = BS(html, 'html.parser')
-            headers = soup.find(id='internet-usage').table.tr
+            headers = soup.find(id='online-private').table.find_all("tr")
+        except AttributeError:
+            self.login()
+            soup = self._get_devices_soup()
 
-            entries = [x for x in headers.next_siblings if x != '\n']
-
-            self.last_results = dict([(e.find(headers='mac-address').text, e.find(headers='host-name').text) for e in entries])
+        try:
+            headers = soup.find(id='online-private').table.find_all("tr")
+            for i, h in enumerate(headers):
+                # Ignore first and last row, and blank rows
+                if i == 0 or i == len(headers) - 1 or h == '\n':
+                    continue
+                self.last_results[h.find(id='macaddlocnew').parent.next_sibling] = \
+                  h.find('a', {'class': 'label device-name private'}).text.strip('\t')
         except:
             raise(ValueError())
+
+    def _get_devices_soup(self):
+        html = self.session.get(self.host + CONNECTED_DEVICES_PATH, timeout=TIMEOUT).text
+        return BS(html, 'html.parser')
--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,12 +1,14 @@
 import unittest
 import requests_mock
 import requests
-from xfinity_gateway import XfinityGateway
+from xfinity_gateway import XfinityGateway, CONNECTED_DEVICES_PATH, LOGIN_PATH
 
 @requests_mock.Mocker()
 class TestXfinity(unittest.TestCase):
 
     URL = 'http://10.0.0.1'
+    USERNAME = 'Colby'
+    PASSWORD = 'hunter2'
     CONNECTED_DEVICES = {'A0:F3:C1:00:00:00': 'TPLINK', '18:65:90:00:00:00': 'Colbys-iPhone', '48:A9:1C:00:00:00': 'Carloss-iPhone', '70:56:81:00:00:00': 'Colbys-Air', 'CC:9E:A2:00:00:00': 'amazon-4d73d5f06', '20:EE:28:00:00:00': 'Amandas-iPhone', '80:E6:50:00:00:00': 'Amandas-MBP-2'}
 
     @classmethod
@@ -16,21 +18,24 @@ class TestXfinity(unittest.TestCase):
 
 
     def setup_matcher(self, m):
-        m.get(self.URL, text=self.test_xfinity_page)
-        m.get('http://10.0.0.2', exc=requests.exceptions.ConnectTimeout)
-        m.get('http://10.0.0.3', text='<html></html>')
+        m.get(self.URL + CONNECTED_DEVICES_PATH, text=self.test_xfinity_page)
+        m.post(self.URL + LOGIN_PATH)
+        m.get('http://10.0.0.2' + CONNECTED_DEVICES_PATH, exc=requests.exceptions.ConnectTimeout)
+        m.post('http://10.0.0.2' + LOGIN_PATH)
+        m.get('http://10.0.0.3' + CONNECTED_DEVICES_PATH, text='<html></html>')
+        m.post('http://10.0.0.3' + LOGIN_PATH)
 
     def test_scan_devices(self, m):
         self.setup_matcher(m)
 
-        gateway = XfinityGateway('10.0.0.1')
+        gateway = XfinityGateway('10.0.0.1', self.USERNAME, self.PASSWORD)
         
         self.assertEqual(gateway.scan_devices(), self.CONNECTED_DEVICES.keys())
 
     def test_get_device_name_valid(self, m):
         self.setup_matcher(m)
 
-        gateway = XfinityGateway('10.0.0.1')
+        gateway = XfinityGateway('10.0.0.1', self.USERNAME, self.PASSWORD)
         gateway.scan_devices()
 
         self.assertEqual(gateway.get_device_name('18:65:90:00:00:00'), 'Colbys-iPhone')
@@ -38,7 +43,7 @@ class TestXfinity(unittest.TestCase):
     def test_invalid_host(self, m):
         self.setup_matcher(m)
 
-        gateway = XfinityGateway('10.0.0.2')
+        gateway = XfinityGateway('10.0.0.2', self.USERNAME, self.PASSWORD)
 
         with self.assertRaises(requests.exceptions.RequestException):
             gateway.scan_devices()
@@ -46,7 +51,7 @@ class TestXfinity(unittest.TestCase):
     def test_wrong_host(self, m):
         self.setup_matcher(m)
 
-        gateway = XfinityGateway('10.0.0.3')
+        gateway = XfinityGateway('10.0.0.3', self.USERNAME, self.PASSWORD)
 
         with self.assertRaises(ValueError):
             gateway.scan_devices()
